e2ed98c5fb95e05af5ea704b1e936cf10d8e8a60
James Scott <jsa.codes@gmail.com>
2023-02-23 09:55:42 -0600

    Added comments to Button.js to clarify what is happening with the className functionality.

--- comps/src/App.js
+++ comps/src/App.js
@@ -2,11 +2,11 @@ import Button from './Button'
 
 function App() {
     return <div>
-        <div><Button>Click Here!</Button></div>
-        <div><Button>Buy Now!</Button></div>
-        <div><Button>See Deal!</Button></div>
-        <div><Button>Hide Ads!</Button></div>
-        <div><Button>Notify Me!</Button></div>
+        <div><Button primary>Click Here!</Button></div>
+        <div><Button secondary>Buy Now!</Button></div>
+        <div><Button success>See Deal!</Button></div>
+        <div><Button warning>Hide Ads!</Button></div>
+        <div><Button danger>Notify Me!</Button></div>
     </div>
 }
 
--- comps/src/Button.js
+++ comps/src/Button.js
@@ -1,11 +1,7 @@
+// Made className singular to prevent confusion with the 'classNames' property
 import className from 'classnames'
 
-const finalClassName = className({
-    'bg-blue-500': true,
-    'text-yellow-500': false,
-})
 
-console.log(finalClassName)
 
 
 function Button({
@@ -15,10 +11,22 @@ function Button({
     success,
     warning,
     danger,
-    outlined,
+    outline,
     rounded
 }) {
-    return <button className='px-3 py-1.5 border border-blue-500 bg-blue-500 text-white'>{children}</button>
+
+    // First argument is universal to all buttons
+    // Second argument is a list of each individual button class.
+    const classes = className('px-3 py-1.5 border', {
+        'border-blue-500 bg-blue-500 text-white': primary,
+        'border-gray-900 bg-gray-900 text-white': secondary,
+        'border-green-500 bg-green-500 text-white': success,
+        'border-yellow-400 bg-yellow-400 text-white': warning,
+        'border-red-500 bg-red-500 text-white': danger,
+
+    })
+
+    return <button className={classes}>{children}</button>
 }
 
 Button.propTypes = {
