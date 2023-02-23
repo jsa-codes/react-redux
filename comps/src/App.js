e8ea54f3f19081a3ea9353bfffda5bedc3dbaf32
James Scott <jsa.codes@gmail.com>
2023-02-22 21:25:30 -0600

    Initial commit, plus...initial setup and create Button Component using children passed down from App

---
+++ comps/src/App.js
@@ -0,0 +1,13 @@
+import Button from './Button'
+
+function App() {
+    return <div>
+        <div><Button>Click Here!</Button></div>
+        <div><Button>Buy Now!</Button></div>
+        <div><Button>See Deal!</Button></div>
+        <div><Button>Hide Ads!</Button></div>
+        <div><Button>Notify Me!</Button></div>
+    </div>
+}
+
+export default App
