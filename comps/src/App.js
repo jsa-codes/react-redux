37936e03c86352a8023ed1ec70ce4b7f54a17e12
James Scott <jsa.codes@gmail.com>
2023-02-23 08:33:00 -0600

    Installed PropTypes and created, inside of Button.js, a way to make sure that only one button type is used on any given button. PropTypes is an interesting concept. Now have a better understanding after going through this lesson in Stephen's course.

--- comps/src/App.js
+++ comps/src/App.js
@@ -2,7 +2,7 @@ import Button from './Button'
 
 function App() {
     return <div>
-        <div><Button>Click Here!</Button></div>
+        <div><Button success primary>Click Here!</Button></div>
         <div><Button>Buy Now!</Button></div>
         <div><Button>See Deal!</Button></div>
         <div><Button>Hide Ads!</Button></div>
--- comps/src/Button.js
+++ comps/src/Button.js
@@ -1,14 +1,30 @@
+import PropTypes from "prop-types";
+
 function Button({
-    children, 
-    primary, 
-    secondary, 
-    success, 
-    warning, 
-    danger, 
-    outlined, 
+    children,
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outlined,
     rounded
 }) {
     return <button>{children}</button>
+}
+
+Button.propTypes = {
+    checkButtonType: ({ primary, secondary, success, warning, danger }) => {
+        const count = Number(!!primary)
+            + Number(!!secondary)
+            + Number(!!success)
+            + Number(!!warning)
+            + Number(!!danger)
+
+        if (count > 1) {
+            return new Error('Only one of primary, secondary, success, warning, danger can be true.')
+        }
+    }
 }
 
 export default Button
