c8e52a116d633960f1042c718bd71651e5d21879
James Scott <jsa.codes@gmail.com>
2023-02-23 12:59:42 -0600

    Learning about ...rest JS feature. Very interesting. A great way to pass down functionality to children, like buttons. Makes it very easy to add functionality without passing around a million props. Also learned how to take advantage of React icons.

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,12 +1,21 @@
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go'
 import Button from './Button'
 
 function App() {
+
+    const handleClick = () => {
+        console.log('Clicked!')
+    }
     return <div>
-        <div><Button primary>Click Here!</Button></div>
-        <div><Button secondary>Buy Now!</Button></div>
-        <div><Button success>See Deal!</Button></div>
+        <div>
+            <Button primary onClick={handleClick}>
+                Click Here!
+            </Button>
+        </div>
+        <div><Button secondary><GoDatabase/> Buy Now!</Button></div>
+        <div><Button success rounded><GoCloudDownload/>See Deal!</Button></div>
         <div><Button warning>Hide Ads!</Button></div>
-        <div><Button danger>Notify Me!</Button></div>
+        <div><Button danger><GoBell />Notify Me!</Button></div>
     </div>
 }
 
