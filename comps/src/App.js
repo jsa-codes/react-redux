3c56028077cf6dc5dec912911189f5890d234875
James Scott <jsa.codes@gmail.com>
2023-03-01 14:05:06 -0600

    Revisiting logic for creating dropdowns

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,27 +1,13 @@
-import Accordion from './components/Accordion'
-
+import Dropdown from './components/Dropdown'
 
 function App() {
 
-     const items = [
-        {
-            id: '2347t',
-            label: 'Can I use React on a piece of bread?',
-            content: 'YES. You can use React on a piece of bread'
-        },
-        {
-            id: 'eruy833',
-            label: 'Can I use JSON on a piece of bread?',
-            content: 'YES. You can use JSON on a piece of bread'
-        },
-        {
-            id: '1238457d',
-            label: 'Can I use Peanut butter on a piece of bread?',
-            content: 'YES! You can use Peanut butter on a piece of bread'
-        },
-     ]
-
-    return <Accordion items={items}/>
+    const options = [
+        { label: 'Red', value: 'red' },
+        { label: 'Green', value: 'green' },
+        { label: 'Blue', value: 'blue' },
+    ]
+    return <Dropdown options={options}/>     
     
 }
 
@@ -29,16 +15,3 @@ export default App
 
 
 
-/* 
-PROPS for Buttons : ALL booleans
-(for false just leave button blank)
-
-- primary
-- secondary
-- success
-- warning
-- danger
-
-- outline
-- rounded
-*/
---
+++ comps/src/pages/AccordionPage.js
@@ -0,0 +1,44 @@
+import Accordion from '../components/Accordion'
+
+
+function AccordionPage() {
+
+    const items = [
+        {
+            id: '2347t',
+            label: 'Can I use React on a piece of bread?',
+            content: 'YES. You can use React on a piece of bread'
+        },
+        {
+            id: 'eruy833',
+            label: 'Can I use JSON on a piece of bread?',
+            content: 'YES. You can use JSON on a piece of bread'
+        },
+        {
+            id: '1238457d',
+            label: 'Can I use Peanut butter on a piece of bread?',
+            content: 'YES! You can use Peanut butter on a piece of bread'
+        },
+    ]
+
+    return <Accordion items={items} />
+
+}
+
+export default AccordionPage
+
+
+
+/* 
+PROPS for Buttons : ALL booleans
+(for false just leave button blank)
+
+- primary
+- secondary
+- success
+- warning
+- danger
+
+- outline
+- rounded
+*/
