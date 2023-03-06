b4b73cd452de6821d7c2dcf0e47b278230486777
James Scott <jsa.codes@gmail.com>
2023-03-06 12:57:00 -0600

    In App.js, created handleClick for dropdown and passed props into the Dropdown component. Also created onClick in Dropdown component for rendering the label of the color selected, using optional chaining.

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,14 +1,31 @@
+import { useState } from 'react'
 import Dropdown from './components/Dropdown'
 
 function App() {
+
+    // Default value is null because by default nothing is selected
+    const [selection, setSelection] = useState(null)
+
+
+    // The option the user selected is passed in as the argument
+    const handleSelect = (option) => {
+
+        // Update our selection piece of state
+        setSelection(option)
+    }
 
     const options = [
         { label: 'Red', value: 'red' },
         { label: 'Green', value: 'green' },
         { label: 'Blue', value: 'blue' },
     ]
-    return <Dropdown options={options}/>     
-    
+
+    return <Dropdown
+        options={options}
+        selection={selection}
+        onSelect={handleSelect}
+    />
+
 }
 
 export default App
--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,7 +1,46 @@
+import { useState } from 'react'
 
 
-function Dropdown({ options }) {
-    return <div>Dropdown...</div>
+function Dropdown({ options, selection, onSelect }) {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleClick = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const handleOptionClick = (option) => {
+        // Close the dropdown
+        setIsOpen(false)
+        // Set the option the user clicked on
+        onSelect(option)
+    }
+
+    const renderedOptions = options.map((option) => {
+        return (
+            <div onClick={() => handleOptionClick(option)} key={option.value}>
+                {option.label}
+            </div>
+        )
+    })
+
+    // THIS CODE WAS REPLACED WITH THE CODE BELOW IN THE ONCLICK.
+    // let content = 'Select...'
+    // // If we don't have a selection we DO NOT enter the if statement
+    // if (selection) {
+    //     content = selection.label
+    // }
+
+    return (
+        <div>
+            <div onClick={handleClick}>
+                {selection?.label || 'Select...'}
+            </div>
+            {isOpen && <div>{renderedOptions}</div>}
+        </div>
+    )
 }
 
 export default Dropdown
+
+
