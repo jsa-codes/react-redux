fa0d0988dbfc598e39b3f5b0f900562d00a3c89c
James Scott <jsa.codes@gmail.com>
2023-03-07 11:53:46 -0600

    Created cleanUp function inside of useEffect. Implemented useRef

--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { GoChevronDown } from 'react-icons/go'
 import Panel from './Panel'
 
 
 function Dropdown({ options, value, onChange }) {
 
+    // State declaration
     const [isOpen, setIsOpen] = useState(false)
+    // divEl is a reference to some div element
+    const divEl = useRef()
+
+    useEffect(() => {
+        const handler = (e) => {
+            if (!divEl.current.contains(e.target)) {
+                setIsOpen(false)
+            }
+        }
+        // Watch for a click, whenever a click occurs we run our handler AND watch for clicks during the capture phase
+        document.addEventListener('click', handler, true)
+
+        return () => {
+            document.removeEventListener('click', handler)
+        }
+    }, []
+    )
+
+
 
     // This function is called when we click on a dropdown
     //  The value of isOpen is then set to true
@@ -32,7 +52,7 @@ function Dropdown({ options, value, onChange }) {
         )
     })
 
-    // THIS CODE WAS REPLACED WITH THE CODE BELOW IN THE ONCLICK.
+    // THIS CODE WAS REPLACED WITH THE CODE BELOW IN THE ONCLICK. 
     // let content = 'Select...'
     // // If we don't have a selection we DO NOT enter the if statement
     // if (selection) {
@@ -40,7 +60,7 @@ function Dropdown({ options, value, onChange }) {
     // }
 
     return (
-        <div className='w-48 relative'>
+        <div ref={divEl} className='w-48 relative'>
             <Panel className='flex justify-between items-center cursor-pointer' onClick={handleClick}>
                 {value?.label || 'Select...'} <GoChevronDown className='text-lg' />
             </Panel>
