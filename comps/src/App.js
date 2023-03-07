197dcf1e98ae2bb3a8b4dd870b33c0506de7fb12
James Scott <jsa.codes@gmail.com>
2023-03-07 12:48:47 -0600

    Created DropdownPage component. Copy pasta code from App component into DropdownPage component. Removed all code from App.js for new project.

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,32 +1,7 @@
-import { useState } from 'react'
-import Dropdown from './components/Dropdown'
 
 function App() {
-
-    // Default value is null because by default nothing is selected
-    const [selection, setSelection] = useState(null)
-
-
-    // The option the user selected is passed in as the argument
-    const handleSelect = (option) => {
-
-        // Update our selection piece of state
-        setSelection(option)
-    }
-
-    const options = [
-        { label: 'Red', value: 'red' },
-        { label: 'Green', value: 'green' },
-        { label: 'Blue', value: 'blue' },
-    ]
-
-    return (
-    <div className='flex'>
-        <Dropdown options={options} value={selection} onChange={handleSelect}/>
-        <Dropdown options={options} value={selection} onChange={handleSelect}/>
-    </div>
-    )
-
+    return <div>App</div>
+   
 }
 
 export default App
---
+++ comps/src/pages/DropdownPage.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import Dropdown from '../components/Dropdown'
+
+function DropdownPage() {
+
+    // Default value is null because by default nothing is selected
+    const [selection, setSelection] = useState(null)
+
+
+    // The option the user selected is passed in as the argument
+    const handleSelect = (option) => {
+
+        // Update our selection piece of state
+        setSelection(option)
+    }
+
+    const options = [
+        { label: 'Red', value: 'red' },
+        { label: 'Green', value: 'green' },
+        { label: 'Blue', value: 'blue' },
+    ]
+
+    return (
+        <div className='flex'>
+            <Dropdown options={options} value={selection} onChange={handleSelect} />
+        </div>
+    )
+
+}
+
+export default DropdownPage
+
+
+
